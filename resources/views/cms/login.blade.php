aae0b48ef9767486bcff5c7c52fef6d8bfc8da0e
Luay matar <luaymatar2001@gmail.com>
2023-06-21 08:28:56 +0300

    commit3

--- resources/views/cms/login.blade.php
+++ resources/views/cms/login.blade.php
@@ -123,7 +123,7 @@
       remember_me: document.getElementById('remember').checked
     };
     
-    fetch('https://phplaravel-1025967-3619615.cloudwaysapps.com/admin/login',
+    fetch('http://127.0.0.1:8000/admin/login',
      {
       method: 'POST',
       headers: {
@@ -140,7 +140,7 @@
   const message = response.message;
   //  console.log(response);
   if(message == 'success'){
-    window.location.href = 'https://phplaravel-1025967-3619615.cloudwaysapps.com/admin';
+    window.location.href = 'http://127.0.0.1:8000/admin';
   }else{
    
     const codeMessage = ' <div class="alert alert-warning alert-dismissible fade show" role="alert">'+
