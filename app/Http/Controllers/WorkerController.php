9a6581c00af7bb02569420104aa38022d21a4cb1
Luay matar <luaymatar2001@gmail.com>
2023-07-06 18:57:29 +0300

    commit gallert

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -265,7 +265,7 @@ class WorkerController extends Controller
     {
         $worker = Worker::where('slug', $slug)->first();
         $gallery = gallery::owner($worker->id)->with('images')->latest('updated_at')->get();
-        if ($gallery) {
+        if (count($gallery) > 0) {
             return response()->json(['gallery' => $gallery], 200);
         } else {
             return response()->json(['gallery' => 'the gallery is empty'], 400);
